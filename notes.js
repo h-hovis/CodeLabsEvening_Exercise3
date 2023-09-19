c15ae8a373f541c585ed13e4fa8a112d735b9691
hslape <136274044+hslape@users.noreply.github.com>
2023-09-18 20:37:38 -0500

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,21 @@
+// Exercise 3
+
+// Objective - Deepen understanding of DOM selection methods and dynamic interaction by handling multiple elements.
+
+// Instructions:
+
+// 1. Create a new HTML file named multiple_buttons.html. In this file, include at least three buttons with the class btn_onclick. Each button can have unique text like "Button 1", "Button 2", and so on. 
+
+// 2. In the same HTML file, create a <script> section.
+
+// 3. Inside the script element, use the querySelectorAll method to select all buttons with the class btn_onclick.
+
+// 4. Use the forEach method to loop over each button in the node list.
+
+// 5. For each button, add an onclick event that will change the document's background color when the button is clicked.
+
+// 6. Instead of setting a static color like #a7f3d0, make the color dynamic.
+
+// 7. For an added challenge, assign a different color to each button so that when clicked, they change the background color to a unique hue.
+
+// 8. Test your code by opening multiple_buttons.html in a browser. Each button should change the background to a distinct color.
